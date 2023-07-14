73f7c45f9e50268badc62d0f7d84ff263a6f8247
sarrabah <sarrabenarbia1996@gmail.com>
2023-07-14 16:07:18 +0200

    Human Readable Time kata

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args){
-        FindOutlier cnb = new FindOutlier();
-        cnb.test();
+        HumanReadableTime hr = new HumanReadableTime();
+        hr.test();
 
     }
 }
